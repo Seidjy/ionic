d72b1657f9f0ec21dcddaf6ecfc6ce13ecefbaed
Seidjy <samuelpadovangit config --global user.name@hotmail.com>
2018-03-29 20:24:56 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { AboutPage } from '../pages/about/about';
 import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { DetalhesPage } from '../pages/detalhes/detalhes';
+
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+    AboutPage,
+    ListPage,
+    DetalhesPage
   ],
   imports: [
     BrowserModule,
@@ -22,12 +26,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage
+    AboutPage,
+    ListPage,
+    DetalhesPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
